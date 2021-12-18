aa3cf5a6da7155407bf57d2f4d0e2499197d9efa
giridhariradhaa <giridhari.radhaa@gmail.com>
2021-12-18 00:22:29 -0600

    first

---
+++ Go-assignment-1.go
@@ -0,0 +1,14 @@
+How does a computer understands the programming language and shares the output in human readable language:
+
+As we all know, a computer consists of CPU, Monitor, Keyboard, Mouse and all these plays a vital roles in whatever the output we expects, however, the important thing here is to have a programming software being isntalled in the computer.
+
+Lets go ahead and learn how a programming language entered in the computer shares the output in human readable language.
+
+Example:
+
+when a person writes the programming language (Code) in the notepad/VS code and saves the file in the computer with the extension related to the software program (Example test.go/test.java), the saved file will store in the computer's hard disk. 
+
+Then to convert the code into human readble language, we will use some commands in the command promt/terminal to let computer understands the instructions we wrote in the saved go file.
+
+When the computer understands the instructions we have given, it will convert the go file to binary file (This file consists of series of 1's and 0's and it's the only language understandably by all the computers in the world) with the help of go compiler, and send it to the CPU to sort the information and send it back to the operating system to display the data in human readable language on the screen of the computer.
+
