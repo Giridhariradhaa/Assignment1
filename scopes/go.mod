777bd2f5ffee6882e73deb6c774949703e497f9e
giridhariradhaa <giridhari.radhaa@gmail.com>
2022-01-05 12:35:28 -0600

    Functionscope

---
+++ scopes/go.mod
@@ -0,0 +1,3 @@
+module github.com/giridhariradhaa/SCOPES
+
+go 1.17
---
+++ scopes/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"github.com/giridhariradhaa/SCOPES/scopes"
+)
+
+func main(){
+	fmt.Println()
+	scopes.Funcscope()
+	scopes.Funcexample()
+}
